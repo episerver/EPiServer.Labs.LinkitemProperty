909ac4b12008f2f5ec1c8455d7dc6c1a1954d734
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-06-11 12:37:30 +0200

    Remove yarn from setup

--- setup.cmd
+++ setup.cmd
@@ -3,14 +3,6 @@ SETLOCAL
 
 SET PATH=.\.ci\tools\;.\build\tools\;%PATH%
 
-REM Ensure all the node modules are installed.
-CALL yarn install
-IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
-
-REM Run the setup task.
-CALL yarn gulp setup
-IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
-
 SET AlloyMVC=src\AlloyMvcTemplates
 
 IF EXIST %AlloyMVC%\App_Data (
