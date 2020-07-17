fdd9fe9962e7a8cd24542e95843295bf71853903
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-07-17 08:24:12 +0200

    Update version to 0.1.2

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("linkitem-property")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyInformationalVersion("0.1.1-developerbuild")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
